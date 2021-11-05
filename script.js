ddc396233b0f0e349b817254d6cd3da3f9eac3d8
Chad Linthicum <portableanswers@gmail.com>
2021-11-05 17:05:27 -0400

    To do list working

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-const output = document.querySelector('#toDoItem');
+const output = document.getElementById('toDoText'); //Textbox input item
+const submitButton = document.getElementById('submit-button'); //Button input item
 
-console.log(output);
+submitButton.addEventListener('click', e => {
+  e.preventDefault(); //dont refresh the browser
+  const todo = output.value; //only works with input elements
+  var li = document.createElement('li');
+  li.innerText = todo;
+  document.body.appendChild(li);
+})
 
 
 
+
+
+
+
+
+
+
+// const first = document.getElementById('firstToDo')
+// console.log(first);
+
+// first.style.color='crimson';
+
+// const lis = document.querySelectorAll('li');
+// lis.forEach(li => {
+//   document.write(li.innerText);
+// })
+
+// const lis2 = document.querySelectorAll('li');
+// lis2.forEach(li => {
+//   console.log(li.innerText); //textContent for everything including hidden
+// })
