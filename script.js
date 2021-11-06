bf0b95b56a9b4dd9a05286249f0621c0e02895b8
Chad Linthicum <portableanswers@gmail.com>
2021-11-06 19:00:04 -0400

    Submit and print working as expected

--- script.js
+++ script.js
@@ -2,24 +2,93 @@ const textBox = document.getElementById('textBox');
 const submitButton = document.getElementById('submitButton');
 const deleteButton = document.getElementById('clearButton');
 
+
 submitButton.addEventListener('click', e => { 
   e.preventDefault(); //dont refresh the browser on submit
+  test2();
+  function test2() {
   const textBoxValue = textBox.value; //variable gets the value from output variable
   const toDoItem = document.createElement('li'); //creates a list element
+  i = 1;
+  toDoItem.className = "toDoItem " + i;
+  i++;
   toDoItem.innerText = textBoxValue; //writes the value of to do to the list item
   const listElement = document.getElementById('toDoList');
-  listElement.appendChild(toDoItem); //grab reference to ul, append child to said reference 
+  listElement.appendChild(toDoItem); //grab reference to ul, append child to said reference x
+
+  
+  
+}
 })
 
-deleteButton.addEventListener('click', e => { //make this variable have an event listener
-  e.preventDefault(); //dont refresh the browser on submit
-  var toDoDeleteAction = document.getElementById('li')
-  toDoDeleteAction.remove();
-})
+//
+// for (x = 1; x < 100; x++){
+//   console.log(x);
+// }
 
-const test = document.getElementById('clearButton');
-console.log(test.innerText); 
+//
+// const test = document.getElementById('clearButton');
+// var content = test.innerText; 
+// console.log(content);
 
+//
+// const links = document.getElementsByTagName("li"); //grabs all anchor tags from HTML file
+// for (i=1; i <= links.length; i++) {
+//   console.log("This is link number "+i);
+// }
+
+//
+// chadL = {name:"Chad Linthicum", age:31};
+// console.log(chadL.age);
+
+// function person (name, age){
+//   this.name = name;
+//   this.age = age;
+// }
+
+// var chad = new person("Chad Linthicum", 31); //object name and blue print you want to use
+// var emily = new person("Emily Cullen", 31.5); 
+// console.log(chad.name);
+// console.log(emily.name);
+// console.log(emily.age)
+
+//
+// var recipe = {title:"Mole", servings:2, ingredients:["cinnamon", "cumin", "cocoa"]};
+// console.log(recipe.title);
+// console.log(recipe.servings);
+// console.log(recipe.ingredients);
+
+// var recipe = {
+//   'title': 'Mole',
+//   'servings': 2,
+//   'ingredients': ['cumin', 'cinnamon', 'cocoa']
+// };
+
+// console.log(recipe.title);
+// console.log('Servings: ' + recipe.servings);
+// console.log('Ingredients:');
+// for (var i = 0; i < recipe.ingredients.length; i++) {
+//   console.log(recipe.ingredients[i]);
+// }
+
+//
+// var books = [
+//   {title: 'The Hobbie', author: 'Tolkien', alreadyRead: false},
+//   {title: 'Metroid: Vol 1', author: 'Nintendo', alreadyRead: true},
+//   {title: "The Holy Bible", author:'God', alreadyRead: true}
+// ];
+
+// for (var z = 0; z < books.length; z++) {
+//   var book = books[z];
+//   var bookInfo = book.title + " by " + book.author;
+//   if (book.alreadyRead) {
+//     console.log('You done read ' + bookInfo);
+//   } else {
+//     console.log('Aint read ' + bookInfo);
+//   }
+// }
+
+//
 // const first = document.getElementById('firstToDo')
 // console.log(first);
 
